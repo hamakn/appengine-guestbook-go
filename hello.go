5a15480e4172d6f7e62d19b9564c9b3f1dc286a2
Katsunori Kawaguchi <hamakata@gmail.com>
2017-10-10 00:06:54 +0900

    Implement Google Accounts Authentication
    https://cloud.google.com/appengine/docs/standard/go/users/

--- hello.go
+++ hello.go
@@ -23,11 +23,23 @@ type Greeting struct {
 func init() {
 	http.HandleFunc("/", root)
 	http.HandleFunc("/sign", sign)
+	http.HandleFunc("/welcome", welcome)
 }
 
 // guestbookKey returns the key used for all guestbook entries.
 func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	ctx := appengine.NewContext(r)
+	u := user.Current(ctx)
+	if u == nil {
+		url, _ := user.LoginURL(ctx, "/")
+		fmt.Fprintf(w, `<a href="%s">Sign in or register</a>`, url)
+		return
+	}
+	url, _ := user.LogoutURL(ctx, "/")
+	fmt.Fprintf(w, `Welcome, %s! (<a href="%s">sign out</a>)`, u, url)
 }
+
 func guestbookKey(c appengine.Context) *datastore.Key {
 	// The string "default_guestbook" here could be varied to have multiple guestbooks.
 	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
